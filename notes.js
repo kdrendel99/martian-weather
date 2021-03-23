55302f1f5fbba75afbc5e289d45d02e587ecd742
Karlson Drendel <kdrendel99@gmail.com>
2021-03-22 20:19:43 -0700

    Built out functionality for second API call, add image caption and image to homepage.

---
+++ notes.js
@@ -0,0 +1,14 @@
+    // function getImg(){
+    //   return fetch (`https://api.nasa.gov/planetary/apod?api_key=H7qr70Zy6DmIvbTSaxPjqkHUQWuWfg6FNIj9syic`)
+    // .then(function(response){
+    //   if (!response.ok) {
+    //     throw Error(response.statusText);
+    //   }
+    //   return response.json();
+    // })
+    // .catch(function(error){
+    //   return error;
+    // })}
+    // getImg();
+    // }
+  // });
--- src/main.js
+++ src/main.js
@@ -8,9 +8,10 @@ $(document).ready(function() {
 
     let request = new XMLHttpRequest();
     const url = 'https://api.nasa.gov/insight_weather/?api_key=H7qr70Zy6DmIvbTSaxPjqkHUQWuWfg6FNIj9syic&feedtype=json&ver=1.0 ';
-    request.onreadystatechange = function () {
+    request.onreadystatechange = function () 
+    {
       if (this.readyState === 4 && this.status === 200) {
-        console.log("It worked!")
+        console.log("First API call worked!")
         const response = JSON.parse(this.responseText);
         getElements (response);
       }
@@ -19,9 +20,24 @@ $(document).ready(function() {
     request.send();
     function getElements(response) {
       $('.showInfo').text(`The season on Mars is ${response[response.sol_keys[2]].Season}`);
-      
       $('.showInfo').append(` and the average number of samples taken from on Mars is ${response[response.sol_keys[2]].PRE.av}`);
     }
-  });
 
-
+    let imgRequest = new XMLHttpRequest();
+    const imgUrl = 'https://api.nasa.gov/planetary/apod?api_key=H7qr70Zy6DmIvbTSaxPjqkHUQWuWfg6FNIj9syic';
+    console.log(this.status);
+    imgRequest.onreadystatechange = function ()
+    {
+      if (this.readyState === 4 && this.status === 200) {
+        console.log("Second API call worked!")
+      const response2 = JSON.parse(this.responseText);
+      getImage (response2);
+    }
+  }
+  imgRequest.open ("GET", imgUrl, true);
+  imgRequest.send();
+  function getImage(response2)  {
+    $('.showImage').html(`${response2.explanation}`)
+    $('.showImage').append(`<img src="${response2["url"]}"width="400">`);
+  }
+    });
